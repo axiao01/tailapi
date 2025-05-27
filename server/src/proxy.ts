3bdcfade9215760914b4f1e8ca8d4ccfc7f33cfa
Angela Xiao <angela.yxiao@gmail.com>
2025-05-26 22:20:38 -0400

    init

--- server/src/proxy.ts
+++ server/src/proxy.ts
@@ -2,16 +2,75 @@
 import { Proxy } from "http-mitm-proxy";
 import { RequestModel } from "./models/Request";
 import { v4 as uuidv4 } from "uuid";
-
+import net from "net";
+import { exec } from "child_process";
+import assert from "assert";
 // Extend the IContext type to include requestBody and responseBody
+
+// Since node 0.9.9, ECONNRESET on sockets are no longer hidden
+function filterSocketConnReset(err: any, socketDescription: string) {
+  if (err.errno === "ECONNRESET") {
+    console.log(`Got ECONNRESET on ${socketDescription}, ignoring.`);
+  } else {
+    console.log(`Got unexpected error on ${socketDescription}`, err);
+  }
+}
+
 export function createProxyServer() {
   const proxy = new Proxy();
+  proxy.onConnect((req, socket, head, callback) => {
+    console.log(`HTTPS CONNECT to ${req?.url}`, head.toString("utf-8"));
+    if (!req.url) {
+      console.log("No url in request");
+      return;
+    }
+    //return callback(); // continue with MITM
+    const host = req.url.split(":")[0];
+    const port = parseInt(req.url.split(":")[1]);
 
-  proxy.onError((ctx: any, err: any) => {
+    console.log("Tunnel to", req.url);
+    const conn = net.connect(
+      {
+        port,
+        host,
+        allowHalfOpen: true,
+      },
+      () => {
+        conn.on("finish", () => {
+          socket.destroy();
+        });
+        socket.on("close", () => {
+          conn.end();
+        });
+        socket.write("HTTP/1.1 200 OK\r\n\r\n", "utf-8", () => {
+          conn.pipe(socket);
+          socket.pipe(conn);
+        });
+        socket.write("HTTP/1.1 200 OK\r\n\r\n", "utf-8", () => {
+          conn.pipe(socket);
+          socket.pipe(conn);
+        });
+      }
+    );
+
+    conn.on("error", (err) => {
+      filterSocketConnReset(err, "PROXY_TO_SERVER_SOCKET");
+    });
+    socket.on("error", (err) => {
+      filterSocketConnReset(err, "CLIENT_TO_PROXY_SOCKET");
+    });
+  });
+  /*proxy.onError((ctx: any, err: any) => {
+    console.log(
+      ctx?.clientToProxyRequest?.url,
+      "---------error------------",
+      ctx
+    );
     console.error("Proxy error:", err);
   });
 
   proxy.onRequest((ctx: any, callback) => {
+    console.log(ctx?.clientToProxyRequest?.url, "---------------------");
     const chunks: Buffer[] = [];
 
     ctx.onRequestData((ctx: any, chunk: any, cb: any) => {
@@ -54,8 +113,21 @@ export function createProxyServer() {
     });
 
     callback();
+  });*/
+  const port = 8888;
+  proxy.listen({ port }, () => {
+    console.log("✅ Proxy server started on port ", port);
+    const cmd = `curl -x http://localhost:${port} https://github.com/ | grep html`;
+    console.log(`> ${cmd}`);
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`${stdout}`);
+      //assert(/DOCTYPE/.test(stdout));
+      proxy.close();
+    });
+    //console.log("🕵️ TailAPI proxy running at http://localhost:8888");
   });
-
-  proxy.listen({ port: 8000 });
-  console.log("🕵️ TailAPI proxy running at http://localhost:8000");
 }
